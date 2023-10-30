51389b8fa6f24b1bb89476d4cb67baaaa64a69a7
swatsengels1 <70349220+swatsengels1@users.noreply.github.com>
2023-10-30 21:31:01 +0100

    Working on mandelbrot visualization

--- Engels_14947994_Sahrani_12661651_Siregar_1486305.py
+++ Engels_14947994_Sahrani_12661651_Siregar_1486305.py
@@ -1,3 +1,8 @@
+import math
+import cmath
+import numpy as np
+import matplotlib.pyplot as plt
+
 palette = {
     "green": "8bbf9f",
     "blue": "83bcff",
@@ -5,3 +10,27 @@ palette = {
     "violet": "59344f",
     "crayola": "EE204D"
 }
+    
+def mandelbrot(c, num_set):
+    z = 0
+    for i in range(num_set):
+        z = z**2 + c
+        if abs(z) > 100:
+            return i
+    return num_set
+
+def plot_mandelbrot():
+    x = np.linspace(-2, 2, 1000)
+    y = np.linspace(-2, 2, 1000)
+    mandelbrot_set = np.zeros((len(y), len(x)))
+    
+    for i, x_coord in enumerate(x):
+        for j, y_coord in enumerate(y):
+            c = complex(x_coord, y_coord)
+            val = mandelbrot(c, 100)
+            mandelbrot_set[i, j] = val
+    
+    plt.imshow(mandelbrot_set, cmap='viridis')
+    plt.show()
+            
+plot_mandelbrot()
