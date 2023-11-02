57e6c52c9cd098c779f4d394926025fa134eaa80
swatsengels1 <70349220+swatsengels1@users.noreply.github.com>
2023-11-02 11:44:46 +0100

    Changed colors.

--- Engels_14947994_Sahrani_12661651_Siregar_1486305.py
+++ Engels_14947994_Sahrani_12661651_Siregar_1486305.py
@@ -2,6 +2,25 @@ import math
 import cmath
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from numba import njit, prange
+
+palette = {
+    "green": (139, 191, 159),
+    "blue": (131, 188, 255),
+    "midnight": (18, 69, 89),
+    "violet": (89, 52, 79),
+    "crayola": (238, 32, 77)
+}
+
+normalized_palette = {key: tuple(val / 255.0 for val in value)
+                      for key, value in palette.items()}
+
+colors = list(normalized_palette.values())
+color_stops = [i / (len(colors) - 1) for i in range(len(colors))]
+cmap = LinearSegmentedColormap.from_list(
+    "custom_gradient", list(zip(color_stops, colors)))
+
 
 palette = {
     "green": "8bbf9f",
@@ -15,7 +34,7 @@ def mandelbrot(c, num_set):
     z = 0
     for i in range(num_set):
         z = z**2 + c
-        if abs(z) > 100:
+        if abs(z) > 2:
             return i
     return num_set
 
@@ -30,7 +49,7 @@ def plot_mandelbrot():
             val = mandelbrot(c, 100)
             mandelbrot_set[i, j] = val
     
-    plt.imshow(mandelbrot_set, cmap='viridis')
+    plt.imshow(mandelbrot_set, cmap=cmap)
     plt.ylabel("Real Numbers")
     plt.xlabel("Imaginary Numbers")
     plt.show()
