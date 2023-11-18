fe21d657aafe8d9b06e71d071cf31e5b0ec19d27
AmirSahrani <115920196+AmirSahrani@users.noreply.github.com>
2023-11-18 10:58:45 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -13,10 +13,11 @@ point_estimated = df.groupby(["method", "sample_size"]).aggregate(
 point_estimated["Confidence Interval"] = [stats.t.interval(
     1-alpha, 99, mean, std/33) for mean, std in zip(point_estimated["mean"], point_estimated["std"])]
 print(point_estimated.to_latex(float_format="%.3f"))
-print(point_estimated.head(100))
+print(point_estimated.head(10))
 
 array_like = df.groupby("method")["area"].apply(
     list).reset_index()["area"].values
+methods = df["method"].unique()
 
 levene_stat, levene_pvalue = stats.levene(*array_like)
 print(f"Levene's Test - Statistic: {levene_stat}, p-value: {levene_pvalue}")
@@ -31,15 +32,14 @@ shapiro_stat_group3, shapiro_pvalue_group3 = stats.shapiro(array_like[2])
 shapiro_stat_group4, shapiro_pvalue_group4 = stats.shapiro(array_like[3])
 
 print(
-    f"Shapiro-Wilk Test - Group 1: Statistic: {shapiro_stat_group1}, p-value: {shapiro_pvalue_group1}")
+    f"Shapiro-Wilk Test - {methods[0]}: Statistic: {shapiro_stat_group1}, p-value: {shapiro_pvalue_group1}")
 print(
-    f"Shapiro-Wilk Test - Group 2: Statistic: {shapiro_stat_group2}, p-value: {shapiro_pvalue_group2}")
+    f"Shapiro-Wilk Test - {methods[1]}: Statistic: {shapiro_stat_group2}, p-value: {shapiro_pvalue_group2}")
 print(
-    f"Shapiro-Wilk Test - Group 3: Statistic: {shapiro_stat_group3}, p-value: {shapiro_pvalue_group3}")
+    f"Shapiro-Wilk Test - {methods[2]}: Statistic: {shapiro_stat_group3}, p-value: {shapiro_pvalue_group3}")
 print(
-    f"Shapiro-Wilk Test - Group 4: Statistic: {shapiro_stat_group4}, p-value: {shapiro_pvalue_group4}")
+    f"Shapiro-Wilk Test - {methods[3]}: Statistic: {shapiro_stat_group4}, p-value: {shapiro_pvalue_group4}")
 
-# sample = orthogonal(0,1,1000000)[:,0]
 sample = uniform_square(0,1,1000000)[:,0]
 unif_cdf = np.linspace(0,1,1000000)
 print(stats.ks_2samp(sample,unif_cdf))
