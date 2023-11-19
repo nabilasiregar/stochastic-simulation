f73c34fc2469de4bdb73a92e212cfb9e45f96fed
AmirSahrani <amirsahrani1@gmail.com>
2023-11-19 19:17:27 +0100

    huh?

--- plotting.py
+++ plotting.py
@@ -1,9 +1,17 @@
-from Engels_14947994_Sahrani_12661651_Siregar_1486305 import uniform_square, latin_hypercube, orthogonal, return_cdf
+from Engels_14947994_Sahrani_12661651_Siregar_1486305 import uniform_square, latin_hypercube, orthogonal
 from scipy.stats.qmc import LatinHypercube
 import matplotlib.pyplot as plt
 from numpy import var, linspace
+import numpy as np
 
-n = 7**2
+def return_cdf(samples):
+    """Returns the cumulative distribution function of a list of samples, flattens 2 dimensional lists"""
+    sorted_samples = np.sort(samples)[:,0]
+    cdf = np.cumsum(sorted_samples) / np.sum(sorted_samples)
+    print(cdf)
+    return cdf
+
+n = 10**2
 params = [0, 1, n]
 
 uniform_variances = []
@@ -16,8 +24,6 @@ for i in range(100):
     uniform_variances.append(var(uniform_square(*params)))
     amateur_lhs_variances.append(var(latin_hypercube(*params)))
     amateur_orth_variances.append(var(orthogonal(*params)))
-    pro_lhs_variances.append(var(LatinHypercube(2).random(n)))
-    pro_ortho_variances.append(var(LatinHypercube(2, strength=2).random(n)))
 
 domain = list(range(100))
 palette = {
@@ -35,13 +41,9 @@ fig.suptitle("Comparison of Sampling Methods")
 axs[0].set_title("Variance of the Sampling methods")
 axs[0].plot(domain, uniform_variances, label='Uniform Sampling', color='green')
 axs[0].plot(domain, amateur_lhs_variances,
-            label='Amateur LHS Sampling', color='blue')
+            label='Latin Hypercube Sampling', color='blue')
 axs[0].plot(domain, amateur_orth_variances,
-            label='Amateur Orthogonal Sampling', color='midnightblue')
-axs[0].plot(domain, pro_lhs_variances,
-            label='Pro LHS Sampling', color='violet')
-axs[0].plot(domain, pro_ortho_variances,
-            label='Pro Orthogonal Sampling', color='crimson')
+            label='Orthogonal Sampling', color='midnightblue')
 axs[0].legend()
 
 # Subplot 2
@@ -49,13 +51,9 @@ axs[1].set_title("Cumulative Distribution Functions")
 axs[1].plot(linspace(*params), return_cdf(uniform_square(*params))
             [0], label='Uniform Sampling', color='green')
 axs[1].plot(linspace(*params), return_cdf(latin_hypercube(*params))
-            [0], label='Amateur LHS Sampling', color='blue')
+            [0], label='Laten Hypercube Sampling', color='blue')
 axs[1].plot(linspace(*params), return_cdf(orthogonal(*params))[0],
-            label='Amateur Orthogonal Sampling', color='midnightblue')
-axs[1].plot(linspace(*params), return_cdf(LatinHypercube(2).random(n))
-            [0], label='Pro LHS Sampling', color='violet')
-axs[1].plot(linspace(*params), return_cdf(LatinHypercube(2, strength=2).random(n))
-            [0], label='Pro Orthogonal Sampling', color='crimson')
+            label='Orthogonal Sampling', color='midnightblue')
 axs[1].plot(linspace(*params), linspace(0, 1, n), color="gray")
 axs[1].legend()
 
