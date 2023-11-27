9193791bf22067814d42958d0df08d2a90a132c9
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2023-11-27 15:36:10 +0100

    merge simulation results to one file

--- assignment_2/simulation.py
+++ assignment_2/simulation.py
@@ -6,33 +6,24 @@ results_dir = "simulation_results"
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
 
-def aggregate_results(all_results):
-    aggregated = {'waiting_times': [], 'system_times': [], 'utilization': []}
-    for result in all_results:
-        aggregated['waiting_times'].extend(result['waiting_times'])
-        aggregated['system_times'].extend(result['system_times'])
-        aggregated['utilization'].extend(result['utilization'])
-    return aggregated
-
-def run_simulation(n_servers, num_runs):
-    all_results = []
-    for _ in range(num_runs):
-        simulation = Simulation(lam, mu, n_servers, priority, debug, runtime)
-        results = simulation.run()
-        all_results.append(results)
-    return aggregate_results(all_results)
-
-def save_results_to_csv(results, file_path, run_number):
+def save_results_to_csv(results, file_path, run_number, n_server):
     with open(file_path, 'a', newline='') as csvfile:
-        fieldnames = ['run_number', 'waiting_time', 'system_time', 'utilization']
+        fieldnames = ['n_server', 'run_number', 'waiting_time', 'system_time', 'utilization']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         for wt, st, ut in zip(results['waiting_times'], results['system_times'], results['utilization']):
             writer.writerow({
+                'n_server': n_server,
                 'run_number': run_number,
                 'waiting_time': wt,
                 'system_time': st,
                 'utilization': ut
             })
+
+file_path = os.path.join(results_dir, f'results.csv')
+
+with open(file_path, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=['n_server', 'run_number', 'waiting_time', 'system_time', 'utilization'])
+    writer.writeheader()
 
 # Set parameters for the simulation
 mu = 0.8
@@ -43,15 +34,10 @@ runtime = 1000
 num_runs = 100
 
 for n_servers in [1, 2, 4]:
-    file_path = os.path.join(results_dir, f'results_n_{n_servers}.csv')
-
-    with open(file_path, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['run_number', 'waiting_time', 'system_time', 'utilization'])
-        writer.writeheader()
-
+    print(f"Running simulations for {n_servers} servers...")
     for run_number in range(1, num_runs + 1):
         simulation = Simulation(lam, mu, n_servers, priority, debug, runtime)
         results = simulation.run()
-        save_results_to_csv(results, file_path, run_number)
+        save_results_to_csv(results, file_path, run_number, n_servers)
 
-    print(f'Results for {n_servers} servers saved to {file_path}')
+print(f'Results for simulation saved to {file_path}')
