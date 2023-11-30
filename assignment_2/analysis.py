9d045bc954e4fe73a4b2409315f47bbcb0edaef9
AmirSahrani <amirsahrani1@gmail.com>
2023-11-30 12:13:11 +0100

    configs

---
+++ assignment_2/analysis.py
@@ -0,0 +1,135 @@
+import numpy as np
+import pandas as pd
+import math
+from scipy import stats
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import statsmodels.stats.power as smp
+
+palette = {
+    "green": (139, 191, 159),
+    "blue": (131, 188, 255),
+    "midnight": (18, 69, 89),
+    "violet": (89, 52, 79),
+    "crayola": (238, 32, 77)
+}
+
+normalized_palette = {key: tuple(val / 255.0 for val in value)
+                      for key, value in palette.items()}
+
+colors = list(normalized_palette.values())
+color_stops = [i / (len(colors) - 1) for i in range(len(colors))]
+cmap = LinearSegmentedColormap.from_list(
+    "custom_gradient", list(zip(color_stops, colors)))
+
+
+def statistics(filename):
+    #Collecting the data from the csv
+    f = pd.read_csv(filename, header=0)
+    df = pd.DataFrame(f)
+    
+    mm1_data = df.groupby("n_server").get_group(1)["waiting_time"].values
+    mm2_data = df.groupby("n_server").get_group(2)["waiting_time"].values
+    mm4_data = df.groupby("n_server").get_group(4)["waiting_time"].values
+    
+    mm1_wait_mean = np.mean(mm1_data)
+    mm2_wait_mean = np.mean(mm2_data)
+    mm4_wait_mean = np.mean(mm4_data)
+    
+    mm1_wait_std = np.std(mm1_data)
+    mm2_wait_std = np.std(mm2_data)
+    mm4_wait_std = np.std(mm4_data)
+    
+    methods = ["M/M/1", "M/M/2", "M/M/4"]
+    means = [mm1_wait_mean, mm2_wait_mean, mm4_wait_mean]
+    stds = [mm1_wait_std, mm2_wait_std, mm4_wait_std]
+    
+    #Creating the general stats
+    alpha = 0.01
+    general_stats = df.groupby(["n_server"]).aggregate(["mean", "std", "min", "max"])["waiting_time"]
+    print(general_stats.to_latex(float_format="%.3f"))
+    
+    #ANOVA
+    statistic, p_value = stats.f_oneway(mm1_data, mm2_data, mm4_data)
+    print(f"P_value for ANOVA: {p_value}")
+    print()
+    
+    #T-tests
+    statistic_1, p_value_1 = stats.ttest_ind(a= mm1_data, b=mm2_data, equal_var=True)
+    statistic_2, p_value_2 = stats.ttest_ind(a= mm1_data, b=mm4_data, equal_var=True)
+    statistic_4, p_value_4 = stats.ttest_ind(a= mm2_data, b=mm4_data, equal_var=True)
+    print(f"T-test p_value between n = 1, n = 2:  {p_value_1}")
+    print(f"T-test p_value between n = 1, n = 4:  {p_value_2}")
+    print(f"T-test p_value between n = 2, n = 4:  {p_value_4}")
+    print()
+    
+    #Creating the confidence intervals
+    
+    plt.figure(figsize=(10, 6))
+    
+    for i in range(len(stds)):
+            standard_error = stds[i]/ 10
+            df = 99
+            conf_interval = stats.t.interval(1-alpha, df, means[i], scale=standard_error)
+            rounded_conf_interval = tuple(round(value, 3) for value in conf_interval)
+            print(f"{methods[i]} Confidence Interval: {rounded_conf_interval}")
+            plt.errorbar(x=i, y=means[i], yerr=(conf_interval[1] - means[i]), fmt='o', label=methods[i], color = colors[i])
+
+    plt.xticks(range(len(means)), [methods[i] for i in range(len(means))], fontsize = 12)
+    plt.ylabel('Expected Waiting Time', fontsize = 16)
+    plt.title('Confidence Intervals for Expected Waiting Times', fontsize = 16)
+    plt.show()
+
+#statistics("./assignment_2/simulation_results/results.csv")
+
+def expected_wait_time_n(n, lam, mu):
+    rho = lam/(n* mu)
+    delay_probability = ((n* rho) ** n / math.factorial(n)) * (((1 - rho) *  np.sum([(n * rho) ** m / math.factorial(m) for m in range(n)]) + (n * rho) ** n / math.factorial(n)) ** -1)
+    expected_wait_time_n = delay_probability / (n * mu * (1 - rho))
+    
+    return expected_wait_time_n
+
+def power_analysis(lam, mu):
+    mm1_wait = (lam/(mu)) / (mu - lam)
+    mm2_wait = expected_wait_time_n(2, lam, mu)
+    mm4_wait = expected_wait_time_n(4, lam, mu)
+    
+    var_mm1_wait = (1/mm1_wait)**2
+    var_mm2_wait = (1/mm2_wait)**2
+    var_mm4_wait = (1/mm4_wait)**2
+    
+    waiting_time_data = [np.array([mm1_wait]), np.array([mm2_wait]), np.array([mm4_wait])]
+
+    df_between = 2 
+    df_total = 99
+    alpha = 0.05
+    
+    mean_diff_1_2 = mm1_wait - mm2_wait
+    mean_diff_1_4 = mm1_wait - mm4_wait
+    mean_diff_2_4 = mm2_wait - mm4_wait
+
+    pooled_var_1_2 = (99*var_mm1_wait + 99*var_mm2_wait) / (99 + 99 - 2)
+    pooled_sd_1_2 = np.sqrt(pooled_var_1_2)
+    
+    pooled_var_1_4 = (99*var_mm1_wait + 99*var_mm4_wait) / (99 + 99 - 2)
+    pooled_sd_1_4 = np.sqrt(pooled_var_1_4)
+    
+    pooled_var_2_4 = (99*var_mm2_wait + 99*var_mm4_wait) / (99 + 99 - 2)
+    pooled_sd_2_4 = np.sqrt(pooled_var_2_4)
+
+    cohen_d_1_2 = mean_diff_1_2 / pooled_sd_1_2
+    cohen_d_1_4 = mean_diff_1_4 / pooled_sd_1_4
+    cohen_d_2_4 = mean_diff_2_4 / pooled_sd_2_4
+    
+    sample_size_1_2 = smp.FTestAnovaPower().solve_power(effect_size=cohen_d_1_2, alpha=alpha, power=0.8, k_groups=3)
+    sample_size_1_4 = smp.FTestAnovaPower().solve_power(effect_size=cohen_d_1_4, alpha=alpha, power=0.8, k_groups=3)
+    sample_size_2_4 = smp.FTestAnovaPower().solve_power(effect_size=cohen_d_2_4, alpha=alpha, power=0.8, k_groups=3)
+    
+    print(f"Number of samples needed for rho = {round(lam/(mu), 3)}: {int(np.ceil(sample_size_1_2))}")
+    print(f"Number of samples needed for rho = {round(lam/(2*mu), 3)}: {int(np.ceil(sample_size_1_4))}")
+    print(f"Number of samples needed for rho = {round(lam/(4*mu), 3)}: {int(np.ceil(sample_size_2_4))}")
+
+    
+power_analysis(0.28, 0.8)
+    
+    
---
+++ assignment_2/simulation.py
@@ -0,0 +1,56 @@
+from main import Simulation
+import os
+import csv
+import random
+from configs import configs
+
+results_dir = "simulation_results"
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+
+def save_results_to_csv(results, file_path, run_number, n_server, kwargs):
+    with open(file_path, 'a', newline='') as csvfile:
+        fieldnames = ['n_server', 'dist_wait', 'dist_serve', 'priority', 'waiting_time', 'system_time', 'utilization']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for wt, st, ut in zip(results['waiting_times'], results['system_times'], results['utilization']):
+            writer.writerow({
+                'n_server': n_server,
+                'dist_wait': kwargs["dist_wait"].__name__,
+                'dist_serve': kwargs["dist_serve"].__name__,
+                'priority': kwargs["priority"],
+                'waiting_time': wt,
+                'system_time': st,
+                'utilization': ut
+            })
+
+file_path = os.path.join(results_dir, f'results.csv')
+
+with open(file_path, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=['n_server', 'dist_wait', 'dist_serve', 'priority', 'waiting_time', 'system_time', 'utilization'])
+    writer.writeheader()
+
+# Set parameters for the simulation
+# mu = 0.8
+# lam = 0.28
+# priority = True
+# debug = False
+# runtime = 1000
+# dist_wait = random.expovariate
+# dist_serve = random.expovariate
+
+#     print(f"Running simulations for {n_servers} servers...")
+#     for run_number in range(1, num_runs + 1):
+#         simulation = Simulation(lam, mu, dist_wait, dist_serve, n_servers, priority, debug, runtime)
+#         results = simulation.run()
+#         save_results_to_csv(results, file_path, run_number, n_servers)
+
+num_runs = 1
+for experiment in configs():
+    print(f"Running simulations for {experiment}...")
+    for n_servers in [1, 2, 4]:
+        for run_number in range(1, num_runs + 1):
+            simulation = Simulation(**configs()[experiment]['kwargs'], n_servers=n_servers)
+            results = simulation.run()
+            save_results_to_csv(results, file_path, run_number, n_servers, configs()[experiment]['kwargs'])
+
+print(f'Results for simulation saved to {file_path}')
