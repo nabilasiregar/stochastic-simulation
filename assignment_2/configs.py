09f27a2d52e827470d49aa5f9f47673a3986a97f
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2023-12-04 11:47:06 +0100

    add linter

--- assignment_2/configs.py
+++ assignment_2/configs.py
@@ -1,19 +1,29 @@
+"""Importing random module to ensure the simulation results are reproducible"""
 import random
 import numpy as np
 
 def lognormal(x):
-    '''Accepts the lambda from an exponential distribution and returns a lognormal distribution with the same mean and variance'''
+    """Accepts the lambda from an exponential distribution and returns a lognormal distribution with the same mean and variance"""
     s = np.sqrt(1/x**2)
     m = 1/x
     block = np.log((s/m)**2 + 1)
     scaled_variance = np.sqrt(block)
-    scaled_mean = np.log(1/x) - block/ 2
+    scaled_mean = np.log(1/x) - block/2
     return np.random.lognormal(scaled_mean, scaled_variance)
 
 def hyperexponential(x):
+    """Accepts the lambda from an exponential distribution and returns a hyperexponential distribution"""
     return random.expovariate(x) if random.random() < 0.75 else random.expovariate(x*1.5)
 
 def configs():
+    """Configuration to handle passing different arguments to the Simulation function
+
+        Argument values can be updated/re-assigned during the simulation
+
+        Example usage:
+        experiment_config = configs()['experiment_1']['kwargs']
+        experiment_config['lam'] = 0.90
+    """
     return {
         "experiment_1":
         {"name": "M/M/n",
