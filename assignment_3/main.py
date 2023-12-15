8ff309527ee7c7b0b5017ff4eb10743360fd1967
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2023-12-15 16:33:14 +0100

    update variables

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -7,30 +7,28 @@ import matplotlib.pyplot as plt
 def main():
     nodes = read_csv(SMALL_MAP)
     paths = add_paths(SMALL_OPT)
-    testing_length = calculate_path_length(paths, nodes)
+    known_best_length = calculate_path_length(paths, nodes)
 
     # Define parameters for simulated annealing
     initial_temperature = 1000.0
     cooling_factor = 0.995
     stopping_temperature = 0.1
-    stopping_iterations = 1000
-
-    np.random.shuffle(paths.copy())
+    chain_length = 1000
+    starting_path = np.random.permutation(range(1,len(nodes)+1))
 
     #Run Simulated Annealing
     best_path, best_length, iterations, t_list, length_list = sim_annealing(
-        nodes, initial_temperature, cooling_factor, stopping_temperature, stopping_iterations, np.random.permutation(range(1,len(nodes)+1)))
+        nodes, initial_temperature, cooling_factor, stopping_temperature, chain_length, starting_path)
     
     # Display results
-    print(f'Best path length found: {best_length}, provided path length: {testing_length}, found in {iterations} iterations')
+    print(f'Best path length found: {best_length}, provided path length: {known_best_length}, found in {iterations} iterations')
 
     # Optionally, plot the final path
     fig, axs = plt.subplots(1,3)
     fig.set_size_inches(18,6)
     plotmap(nodes, axs[0], paths)
-    axs[0].set_title(f"Optimal Path, length:{testing_length:.2f} ")
-
     plotmap(nodes, axs[1], best_path)
+    axs[0].set_title(f"Optimal Path, length:{known_best_length:.2f} ")
     axs[1].set_title(f"Generated path, length:{best_length:.2f}")
     axs[2].loglog(t_list, length_list, color='pink')
     axs[2].plot(t_list, length_list, color='pink')
